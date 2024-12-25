dc9a38a62b916d0b45065e6ee356651fbd3a7b1a
misha <catmisha12345@gmail.com>
2024-12-25 18:01:37 +0200

    sas

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string ee = "поздровляю ты написал:";
+        while (true)
+        {
+            Console.WriteLine("привит");
+            string gg = Console.ReadLine();
+            Console.WriteLine(ee + gg);
+        }
+    }
+}
